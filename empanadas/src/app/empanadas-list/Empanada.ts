d1fd3b605177a37fe4c2471c2538bc7da998e42d
Romero,Luciana <lu.yael.r@gmail.com>
2024-06-18 14:10:44 -0300

    armado de interface Empanada.ts, contrucción de mock, uso de directivas, estetica de tabla

---
+++ empanadas/src/app/empanadas-list/Empanada.ts
@@ -0,0 +1,8 @@
+export interface Empanada{
+    nombre:string;
+    descripcion:string;
+    precio:number;
+    stock:number;
+    imagen:string;
+    novedad: boolean;
+}
--- empanadas/src/app/empanadas-list/empanadas-list.component.ts
+++ empanadas/src/app/empanadas-list/empanadas-list.component.ts
@@ -1,18 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Empanada } from './Empanada';
+
 
 @Component({
   selector: 'app-empanadas-list',
   templateUrl: './empanadas-list.component.html',
   styleUrl: './empanadas-list.component.scss'
 })
-export class EmpanadasListComponent {
-empanada={
-  "nombre":"Carne",
-  "descripcion": "Carne vacuna, cebolla, morrón, aceituna, condimentos. Cocida frita",
-  "precio": 900,
-  "stock":180,
-  "imagen":"assets/img/carne.jpeg"
-}
+export class EmpanadasListComponent implements OnInit {
+empanadas: Empanada[] = [
+  {
+  nombre:"Carne",
+  descripcion: "Carne vacuna, cebolla, morrón, aceituna, condimentos. Cocida frita",
+  precio: 900,
+  stock:180,
+  imagen:"assets/img/carne.jpeg",
+  novedad: false,
+},
+{
+  nombre:"Pollo",
+  descripcion: "Carne vacuna, cebolla, morrón, aceituna, condimentos. Cocida frita",
+  precio: 800,
+  stock:0,
+  imagen:"assets/img/carne.jpeg",
+  novedad: false,
+},
+{
+  nombre:"Calavaza",
+  descripcion: "Carne vacuna, cebolla, morrón, aceituna, condimentos. Cocida frita",
+  precio: 900,
+  stock:100,
+  imagen:"assets/img/carne.jpeg",
+  novedad: true,
+},
+{
+  nombre:"Morron",
+  descripcion: "Carne vacuna, cebolla, morrón, aceituna, condimentos. Cocida frita",
+  precio: 900,
+  stock:50,
+  imagen:"assets/img/carne.jpeg",
+  novedad: false,
+},
+]
 constructor(){}
 ngOnInit():void{}
 }
